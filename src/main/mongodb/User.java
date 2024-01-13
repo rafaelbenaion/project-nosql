544fd4dae2a47c18a1401175a6e62397afe78504
Rafael Baptista <baprafael@icloud.com>
2024-01-13 02:26:36 +0100

    Setting project base structure

---
+++ src/main/mongodb/User.java
@@ -0,0 +1,26 @@
+package main.mongodb;
+
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+import com.mongodb.MongoClient;
+import com.mongodb.MongoCredential;
+import com.mongodb.DBObject;
+import com.mongodb.BasicDBObject;
+import com.mongodb.DBCollection;
+import com.mongodb.DBCursor;
+import com.mongodb.DB;
+import org.bson.Document;
+import java.util.Arrays;
+import java.util.List;
+import com.mongodb.client.FindIterable;
+import java.util.Iterator;
+import java.util.ArrayList;
+import com.mongodb.client.result.UpdateResult;
+import com.mongodb.client.model.UpdateOptions;
+public class User {
+
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+
+}
